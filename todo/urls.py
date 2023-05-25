29714f3e64432364c2991d1a1ffa2fd94061c50e
Villelligiulia <>
2023-05-25 18:57:54 +0000

    Add the completed task feature that strikes the task once the done checkbox is set to true in the add_task view. add a toggle feature that marks the task or unmark it from the main page

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add-task/', views.AddTask.as_view(), name='add_task'),
     path('update-task/<int:pk>/', views.UpdateTask.as_view(), name='update_task'),
     path('delete-task/<int:pk>/', views.DeleteTask.as_view(), name='delete_task'),
+    path('toggle-task/<int:pk>/', views.ToggleTask.as_view(), name='toggle_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .models import Task
 from django.urls import reverse_lazy
@@ -41,3 +41,11 @@ class DeleteTask(generic.DeleteView):
     template_name = 'delete_task.html'
     context_object_name = 'task_to_delete'
     success_url = reverse_lazy('home')
+
+
+class ToggleTask(generic.View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.done = not task.done
+        task.save()
+        return redirect('home')
