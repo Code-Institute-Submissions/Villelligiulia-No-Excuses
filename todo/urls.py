4b760558012dec47fb142d3827ad56d89db0799b
Villelligiulia <>
2023-05-31 12:52:37 +0000

    rename intro.html as index.html

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", LoginView.as_view(template_name="intro.html"),
+    path("", LoginView.as_view(template_name="index.html"),
          name="login"),
     path("tasks/", views.TaskList.as_view(),
          name="home"),
